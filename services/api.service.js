3b6798580d96b53292fa06e3ba988252e417ac53
Waspish <ostapenko.andreik@gmail.com>
2025-03-23 00:13:11 +0300

    added env variables

--- services/api.service.js
+++ services/api.service.js
@@ -3,7 +3,8 @@ import { getKeyValue, TOKEN_DICTIONARY } from './storage.service.js';
 import axios from 'axios';
 
 const getWeather = async (city) => {
-	const token = await getKeyValue(TOKEN_DICTIONARY.token);
+	const token =
+		process.env.TOKEN ?? (await getKeyValue(TOKEN_DICTIONARY.token));
 	if (!token) {
 		throw new Error('Не задан ключ API, задайте его через -t [API_KEY]');
 	}
@@ -19,6 +20,7 @@ const getWeather = async (city) => {
 			}
 		}
 	);
+	console.log(data);
 	return data;
 	// const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`;
 	// const url = new URL(`https://api.openweathermap.org/data/2.5/weather`);
